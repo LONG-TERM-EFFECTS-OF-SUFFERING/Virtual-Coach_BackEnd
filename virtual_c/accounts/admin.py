a4d32514ffdd728fb4e62fd349a368ce737d68f7
Jose <jose.9496h@gmail.com>
2023-09-25 00:24:47 -0500

    fix: account and routine models updated
    the account its now added to django admin panel, primary key set manually now set automatically with django default primary key, user in routine deleted and replaced with userAccount

--- virtual_c/accounts/admin.py
+++ virtual_c/accounts/admin.py
@@ -1,3 +1,27 @@
+# admin.py
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin  # Import the default UserAdmin
+from .models import UserAccount
 
-# Register your models here.
+class CustomUserAdmin(UserAdmin):
+    list_display = ('email','name', 'is_active', 'is_staff')
+    list_filter = ('is_active', 'is_staff')
+    search_fields = ('email', 'name')
+    ordering = ('email',)
+
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal Info', {'fields': ('name',)}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'groups', 'user_permissions')}),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email','name','is_staff', 'password1', 'password2'),
+        }),
+    )
+
+# Register the CustomUser model with the CustomUserAdmin
+admin.site.register(UserAccount, CustomUserAdmin)
--- virtual_c/routines/models.py
+++ virtual_c/routines/models.py
@@ -1,22 +1,21 @@
 from django.db import models
+from accounts.models import UserAccount as User
 
 # Create your models here.
 
 class Exercise(models.Model):
-    id = models.CharField(max_length=10, primary_key=True)
     name = models.CharField()
     img_url = models.CharField()
-
+    def __str__(self):
+        return self.name 
+    
 class Routine(models.Model):
-    id = models.CharField(max_length=10, primary_key=True)
     time = models.BigIntegerField()
     exercises_number = models.IntegerField()
     exercise = models.ManyToManyField(Exercise, through='Routine_has_exercise', blank=True)
-
-class User(models.Model):
-    id = models.CharField(max_length=10, primary_key=True)
-    password = models.CharField(max_length=10)
-    email = models.CharField(max_length=254, unique=True)
+    def __str__(self):
+        return "routine #"+self.id 
+    
 
 class User_has_Routine(models.Model):
     user = models.ForeignKey(User, on_delete = models.CASCADE)
