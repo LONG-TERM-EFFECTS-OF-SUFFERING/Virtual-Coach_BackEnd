9ae6aff9018af01dd2ef06abe368870baa729c3b
Seb0927 <sebastianidrobo@hotmail.com>
2023-10-09 16:27:34 -0500

    Templates error corrected. - Now the debugger will be working alright!

--- virtual_c/virtual_c/urls.py
+++ virtual_c/virtual_c/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include,re_path
 from django.views.generic import TemplateView
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -26,4 +28,8 @@ urlpatterns = [
 ]
 
 
-urlpatterns += [re_path(r'^.*', TemplateView.as_view(template_name='index.html'))]
+#This line has to be uncommented for production
+#urlpatterns += [re_path(r'^.*', TemplateView.as_view(template_name='index.html'))]
+
+#This line has to be deleted in production
+urlpatterns += staticfiles_urlpatterns()
